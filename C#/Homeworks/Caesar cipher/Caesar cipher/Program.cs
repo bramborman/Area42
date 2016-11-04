2ecf7e23a9598d80de576cabc7d42988004b88cf
Marian Dolinský <mariandolinsky@outlook.com>
2016-11-04 18:08:52 +0100

    Added Caesar cipher homework

---
+++ C#/Homeworks/Caesar cipher/Caesar cipher/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Caesar cipher";
+
+            while (true)
+            {
+                int offset;
+
+                Console.Write("Enter any text: ");
+                string text = Console.ReadLine();
+
+                Console.Write("Enter an offset: ");
+                while (!int.TryParse(Console.ReadLine(), out offset)) ;
+
+                foreach (char ch in text)
+                {
+                    int encrypted = ch + (offset % ('z' - 'a'));
+
+                    if (!char.IsUpper((char)encrypted))
+                    {
+                        if (encrypted < 'a')
+                        {
+                            encrypted += 'z' - 'a' + 1;
+                        }
+                        else if (encrypted > 'z')
+                        {
+                            encrypted -= 'z' - 'a' + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (encrypted < 'A')
+                        {
+                            encrypted += 'Z' - 'A' + 1;
+                        }
+                        else if (encrypted > 'Z')
+                        {
+                            encrypted -= 'Z' - 'A' + 1;
+                        }
+                    }
+
+                    Console.Write((char)encrypted);
+                }
+
+                Console.Write("\n\n");
+            }
+        }
+    }
+}

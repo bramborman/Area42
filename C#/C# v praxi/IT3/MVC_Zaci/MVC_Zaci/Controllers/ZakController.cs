d8b0138a4f1d73d7f8a45db043a0aea7dfbce83a
Marian Dolinský <mariandolinsky@outlook.com>
2016-12-16 18:10:19 +0100

    Added MVC_Zaci project, minor changes

---
+++ C#/C# v praxi/IT3/MVC_Zaci/MVC_Zaci/Controllers/ZakController.cs
@@ -0,0 +1,42 @@
+﻿using MVC_Zaci.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC_Zaci.Controllers
+{
+    public sealed class ZakController : Controller
+    {
+        // GET: Zaci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VypisZaka()
+        {
+            return View(new ZakModel { Jmeno = "Jan", Prijmeni = "Gnymors", JeMuz = false, RokNarozeni = 1998 });
+        }
+
+        public ActionResult VypisZaka2()
+        {
+            return View(new ZakModel { Jmeno = "Jan", Prijmeni = "Gnymors", JeMuz = false, RokNarozeni = 1998 });
+        }
+
+        public ActionResult EditZaka()
+        {
+            return View(new ZakModel { Jmeno = "Jan", Prijmeni = "Gnymors", JeMuz = false, RokNarozeni = 1998 });
+        }
+
+        [HttpPost]
+        public ActionResult EditZaka(ZakModel zakModel)
+        {
+            zakModel.JeMuz = Request.Form[nameof(zakModel.JeMuz)] == "on";
+            return View(zakModel);
+        }
+
+        public ActionResult EditZaka2()
+        {
+            return View(new ZakModel { Jmeno = "Jan", Prijmeni = "Gnymors", JeMuz = false, RokNarozeni = 1998 });
+        }
+    }
+}
---
+++ C#/C# v praxi/IT3/MVC_Zaci/MVC_Zaci/Models/ZakModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Zaci.Models
+{
+    public class ZakModel
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public bool JeMuz { get; set; }
+        public int RokNarozeni { get; set; }
+    }
+}

47d8a78578c34a031c5d1d7eb280ed6b4421c62b
Marian Dolinský <mariandolinsky@outlook.com>
2017-02-23 14:29:01 +0100

    Added StringCentering project

---
+++ C#/Console/StringCentering/StringCentering/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringCentering
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            "lol\nwaaat\ncojeeee".WriteCentered();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Console/StringCentering/StringCentering/StringCentering.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCentering
+{
+    public static class StringCentering
+    {
+        public static int GetLinesCount(this string str)
+        {
+            return str.Count(ch => ch == '\n');
+        }
+
+        public static void WriteCentered(this string str)
+        {
+            Console.CursorTop = (Console.WindowHeight - str.GetLinesCount()) / 2;
+
+            foreach (string s in str.Split('\n'))
+            {
+                Console.CursorLeft = (Console.WindowWidth - s.Length) / 2;
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

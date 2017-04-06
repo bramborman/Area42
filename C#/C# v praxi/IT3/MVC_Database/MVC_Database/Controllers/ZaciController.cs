43f259a1ec81a9d71749eb0098b263b28a8c1187
Marian Dolinský <mariandolinsky@outlook.com>
2017-04-06 19:32:05 +0200

    Added MVC_Database project

---
+++ C#/C# v praxi/IT3/MVC_Database/MVC_Database/Controllers/ZaciController.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Database.AppCode.DB;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC_Database.Controllers
+{
+    public class ZaciController : Controller
+    {
+        public ActionResult Index()
+        {
+            DatabaseSchoolController controller = new DatabaseSchoolController();
+            List<SkolaZaci> zaci = controller.GetAll();
+
+            return View();
+        }
+
+        public ActionResult ZakPrijmeni()
+        {
+            return View(new DatabaseSchoolController().GetZaciByPrijmeni("Gnymors"));
+        }
+    }
+}

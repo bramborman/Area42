1b8922f17842325983a57c80d3694bfc16c10bba
Marian Dolinský <mariandolinsky@outlook.com>
2017-05-23 20:17:24 +0200

    Updated Vibrator - simplification, improvements

--- C#/UWP/Vibrator/Vibrator/App.xaml.cs
+++ C#/UWP/Vibrator/Vibrator/App.xaml.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using Windows.ApplicationModel;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation.Metadata;
+using Windows.Phone.Devices.Notification;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media;
 
 namespace Vibrator
 {
     public sealed partial class App : Application
     {
+        private static readonly VibrationDevice vibrationDevice = VibrationDevice.GetDefault();
+        private static readonly ManualResetEvent resetEvent     = new ManualResetEvent(false);
+        private static readonly Task vibrationTask              = new Task(async () =>
+        {
+            while (true)
+            {
+                resetEvent.WaitOne();
+                vibrationDevice.Vibrate(TimeSpan.FromMilliseconds(200));
+                await Task.Delay(TimeSpan.FromMilliseconds(150));
+            }
+        });
+
+        private bool isVibrating = false;
+
         public App()
         {
             InitializeComponent();
-            Suspending += OnSuspending;
+            vibrationTask.Start();
         }
         
         protected override void OnLaunched(LaunchActivatedEventArgs e)
@@ -24,36 +40,36 @@ namespace Vibrator
             ApplicationView applicationView = ApplicationView.GetForCurrentView();
             applicationView.TryEnterFullScreenMode();
 
-            Frame rootFrame = Window.Current.Content as Frame;
-            
-            if (rootFrame == null)
+            if (!(Window.Current.Content is Border) && !e.PrelaunchActivated)
             {
-                rootFrame = new Frame();
-                rootFrame.NavigationFailed += OnNavigationFailed;
-                
-                Window.Current.Content = rootFrame;
-            }
-            
-            if (!e.PrelaunchActivated)
-            {
-                if (rootFrame.Content == null)
+                Button button = new Button
                 {
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
+                    Style = (Style)Resources["VibrateButton"]
+                };
+
+                button.Click += (buttonClickSender, buttonClickE) =>
+                {
+                    if (isVibrating)
+                    {
+                        vibrationDevice.Cancel();
+                        resetEvent.Reset();
+                    }
+                    else
+                    {
+                        resetEvent.Set();
+                    }
+
+                    isVibrating = !isVibrating;
+                };
+
+                Window.Current.Content = new Border
+                {
+                    Background  = new SolidColorBrush(Colors.Purple),
+                    Child       = button
+                };
 
                 Window.Current.Activate();
             }
-        }
-        
-        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-        
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            deferral.Complete();
         }
     }
 }

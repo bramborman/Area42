b1b6dad00be3135c22c0d7b31076979518c72b8d
Marian Dolinský <mariandolinsky@outlook.com>
2017-10-20 15:40:31 +0200

    Backing up the managed HDD or what

--- C#/Xamarin.Android/MicroBitCarController/MicroBitCarController/MainActivity.cs
+++ C#/Xamarin.Android/MicroBitCarController/MicroBitCarController/MainActivity.cs
@@ -1,18 +1,35 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android;
+using Android.Content.PM;
+using Android.Runtime;
 
 namespace MicroBitCarController
 {
     [Activity(Label = "MicroBitCarController", MainLauncher = true)]
-    public class MainActivity : Activity
+    public sealed class MainActivity : Activity
     {
+        private const int PERMISSION_REQUEST_ID_COARSE_LOCATION = 0;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            RequestPermissions(new string[] { Manifest.Permission.AccessCoarseLocation }, 0);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            if (requestCode != 0)
+            {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+            else
+            {
+
+            }
         }
     }
 }

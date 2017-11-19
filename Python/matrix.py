035d9b4b6817520f1682d3f3ca875f72688c9c9f
Marian Dolinský <mariandolinsky@outlook.com>
2017-11-19 16:32:56 +0100

    Added comments to matrix.py

--- Python/matrix.py
+++ Python/matrix.py
@@ -1,11 +1,9 @@
 class Matrix:
+    """
+    Simple class representing a mathematical matrix
+    """
+
     def __init__(self, cells):
-        if len(cells) < 1:
-            raise ValueError("Matrix must have at least one row.")
-
-        if len(cells[0]) < 1:
-            raise ValueError("Matrix must have at least one column.")
-
         for row in cells:
             if len(row) != len(cells[0]):
                 raise ValueError("Each row of a matrix must be of the same length.")
@@ -14,13 +12,36 @@ class Matrix:
 
     @staticmethod
     def zero_matrix(height, width):
-        return Matrix([[0] * width for i in range(height)])
+        """
+        Returns a zero matrix of the specified size
+
+        Keyword arguments:
+        height -- the height of the new matrix
+        width -- the width of the new matrix
+        """
+
+        if height < 0 or width < 0:
+            raise ValueError("Size must be greater than or equal to 0.")
+
+        return Matrix([[0] * width] * height)
 
     @staticmethod
     def unit_matrix(size):
+        """
+        Returns a unit matrix of the specified size
+
+        Keyword arguments:
+        size -- the size of the new matrix (height and width)
+        """
+
+        if size < 0:
+            raise ValueError("Size must be greater than or equal to 0.")
+
         return Matrix([[1 if i == j else 0 for j in range(size)] for i in range(size)])
 
     def __str__(self):
+        # Comprehensions are really fast - way faster than
+        # appending string to string over and over again
         return "\n".join(" ".join(str(cell) for cell in row) for row in self.__cells)
 
     def __validate_index_parameter(self, index):
@@ -40,20 +61,35 @@ class Matrix:
         self.__cells[index[0]][index[1]] = value
 
     def get_num_rows(self):
+        """
+        Gets the number of rows of the matrix
+        """
         return len(self.__cells)
 
     def get_num_cols(self):
+        """
+        Gets the number of columns of the matrix
+        """
         return len(self.__cells[0])
 
-    def transposition(self):
+    def transpose(self):
+        """
+        Returns a new matrix that's a transposion to the current one
+        """
         return Matrix([[self[j, i] for j in range(self.get_num_rows())] for i in range(self.get_num_cols())])
 
     def is_square(self):
+        """
+        Gets a boolean value specificating whether the matrix is a square matrix
+        """
         return self.get_num_rows() == self.get_num_cols()
 
     def is_symmetric(self):
+        """
+        Gets a boolean value specificating whether the matrix is symmetric
+        """
         # If it's not square it's faster not to transpose this matrix and return immediately
-        return self.is_square() and self == self.transposition()
+        return self.is_square() and self == self.transpose()
 
     def __eq__(self, other_matrix):
         if (self.get_num_rows() != other_matrix.get_num_rows()
@@ -73,7 +109,7 @@ class Matrix:
     def __validate_same_size(self, other_matrix):
         if (self.get_num_rows() != other_matrix.get_num_rows()
            or self.get_num_cols() != other_matrix.get_num_cols()):
-            raise ValueError("Both matrixes mus be of the same size.")
+            raise ValueError("Both matrixes must be of the same size.")
 
     def __add__(self, other_matrix):
         self.__validate_same_size(other_matrix)
@@ -101,5 +137,3 @@ class Matrix:
 
     def __rmul__(self, constant):
         return Matrix([[self[i, j] * constant for j in range(self.get_num_cols())] for i in range(self.get_num_rows())])
-
-print(Matrix([[1, 2, 3], [4, 5, 6]]) * Matrix([[-1, 3, -3, 1], [2, 0, 0, 5], [1, 5, 7, -10]]))

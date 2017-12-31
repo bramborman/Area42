d5a0561fbd2f35fd5d0eea7c899c6b9d7eb14458
Marian Dolinský <mariandolinsky@outlook.com>
2017-12-31 23:29:38 +0100

    Improvements :3

--- Python/laws.py
+++ Python/laws.py
@@ -42,6 +42,39 @@ def save_data(data):
             output.write(item)
 
 
+def sort(list):
+    output = [None] * len(list)
+    counts = {}
+
+    # print("counting . . .")
+
+    for item in list:
+        counts[item[0]] = 1 + counts.get(item[0], 0)
+
+    indexes = {}
+    i = 0
+    j = 0
+    shift = 0
+
+    # print("indexing . . .")
+
+    while j != len(counts):
+        if i in counts:
+            indexes[i] = shift
+            shift += counts[i]
+            j += 1
+
+        i += 1
+
+    # print("sorting . . .")
+
+    for item in list:
+        output[indexes[item[0]]] = item[1]
+        indexes[item[0]] += 1
+
+    return output
+
+
 read_start = datetime.now()
 
 list = get_data()
@@ -49,39 +82,12 @@ list = get_data()
 start = datetime.now()
 print("Reading:", start - read_start)
 
-output = [None] * len(list)
-counts = {}
-
-print("counting . . .")
-
-for item in list:
-    counts[item[0]] = 1 + counts.get(item[0], 0)
-
-indexes = {}
-i = 0
-j = 0
-shift = 0
-
-print("indexing . . .")
-
-while j != len(counts):
-    if i in counts:
-        indexes[i] = shift
-        shift += counts[i]
-        j += 1
-
-    i += 1
-
-print("sorting . . .")
-
-for item in list:
-    output[indexes[item[0]]] = item[1]
-    indexes[item[0]] += 1
+sorted = sort(list)
 
 end = datetime.now()
 print("Sorting:", end - start)
 
-save_data(output)
+save_data(sorted)
 
 write_end = datetime.now()
 print("Writing:", write_end - end)

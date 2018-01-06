73928eba8d1864b4f86da7f00b59d370d5bf99c0
Marian Dolinský <mariandolinsky@outlook.com>
2018-01-07 00:47:48 +0100

    Added Challenge001

---
+++ C#/Console/Challenge001/Challenge001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge001
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int[]> list = new List<int[]>()
+            {
+                new [] {1, 2, 3, 4, 5},
+                new [] {1, 2, 3, 4, 5},
+                new [] {1, 2, 3, 4, 5}
+            };
+
+            float[] avgs = new Program().A(list);
+
+            foreach (float f in avgs)
+            {
+                Console.Write(f + ", ");
+            }
+
+            Console.ReadLine();
+        }
+        
+        float[]A(List<int[]>l)=>Enumerable.Range(0,l[0].Length).Select(i=>{float r=0;l.ForEach(a=>r+=a[i]/(float)l.Count);return r;}).ToArray();
+    }
+}

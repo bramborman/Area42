73ba4ddf24d5429da13148682984024e58e8e379
Marian Dolinský <mariandolinsky@outlook.com>
2018-01-07 13:04:17 +0100

    Improved Challenges project

--- C#/Console/Challenges/Challenges/Program.cs
+++ C#/Console/Challenges/Challenges/Program.cs
@@ -10,38 +10,40 @@ namespace Challenges
     {
         public static void Main(string[] args)
         {
-            IEnumerable<TypeInfo> challenges = Assembly.GetExecutingAssembly().DefinedTypes.Where(t => t.GetInterface(nameof(IChallenge)) != null);
+            IEnumerable<string> challenges = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.GetInterface(nameof(IChallenge)) != null).Select(t => t.Name);
 
             while (true)
             {
                 Console.Clear();
 
-                foreach (TypeInfo challenge in challenges)
+                foreach (string challenge in challenges)
                 {
-                    Console.WriteLine(challenge.Name);
+                    Console.WriteLine(challenge);
                 }
 
                 Console.Write("Enter number of a selected challenge or 'exit' to exit: ");
 
                 string input = Console.ReadLine();
 
-                if (Regex.IsMatch(input, "^[0-9]{3}$"))
+                if (Regex.IsMatch(input, "^[0-9]{0,3}$"))
                 {
                     Console.Clear();
+                    
+                    Type challengeType = Type.GetType(nameof(Challenges) + ".Challenge" + input.PadLeft(3, '0'));
 
-                    string challengeTypeName = "Challenge" + input;
-                    Type challengeType = challenges.First(t => t.Name == challengeTypeName).UnderlyingSystemType;
+                    if (challengeType != null)
+                    {
+                        IChallenge challenge = (IChallenge)Activator.CreateInstance(challengeType);
 
-                    IChallenge challenge = (IChallenge)Activator.CreateInstance(challengeType);
+                        Console.Write(new string('=', Console.WindowWidth));
+                        Console.CursorLeft = (Console.WindowWidth - challengeType.Name.Length) / 2;
+                        Console.WriteLine(challengeType.Name.ToUpper());
+                        Console.WriteLine();
+                        Console.WriteLine(challenge.Description);
+                        Console.WriteLine(new string('=', Console.WindowWidth));
 
-                    Console.Write(new string('=', Console.WindowWidth));
-                    Console.CursorLeft = (Console.WindowWidth - challengeType.Name.Length) / 2;
-                    Console.WriteLine(challengeType.Name.ToUpper());
-                    Console.WriteLine();
-                    Console.WriteLine(challenge.Description);
-                    Console.WriteLine(new string('=', Console.WindowWidth));
-
-                    challenge.Run();
+                        challenge.Run();
+                    }
                 }
                 else if (string.Equals(input, "exit", StringComparison.InvariantCultureIgnoreCase))
                 {

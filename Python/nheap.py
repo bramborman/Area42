c417be7fdc7518672824e2c3277094a344e71653
Marian Dolinský <mariandolinsky@outlook.com>
2018-02-11 11:36:59 +0100

    Added nheap base

---
+++ Python/nheap.py
@@ -0,0 +1,25 @@
+"""N-ary heap implementation."""
+
+
+class NHeap(object):
+    """Represents a maximum n-ary heap."""
+
+    def __init__(self, n, hl=None):
+        """Build heap from list. O(n)."""
+        pass
+
+    def get_max(self):
+        """Get maximum item, do not remove it. O(1)."""
+        pass
+
+    def extract_max(self):
+        """Get maximum item and remove it. O(???)."""
+        pass
+
+    def insert(self, value):
+        """Insert new item into heap. O(???)."""
+        pass
+
+    def size(self):
+        """Get size of heap. O(1)."""
+        pass

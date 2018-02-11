1c7d49dc843c2a26d0d71dc18bc796675e07a97f
Marian Dolinský <mariandolinsky@outlook.com>
2018-02-11 15:05:33 +0100

    Improved nheap

--- Python/nheap.py
+++ Python/nheap.py
@@ -1,10 +1,24 @@
-"""N-ary heap implementation."""
+"""
+N-ary heap implementation.
+"""
 
 class NHeap(object):
-    """Represents a maximum n-ary heap."""
+    """
+    Class representing a maximum n-ary heap.
+    """
 
     def __init__(self, n, hl=None):
-        """Build heap from list. O(n)."""
+        """
+        Build heap from list. O(n).
+
+        Keyword arguments:
+        n -- maximum number of children each (sub)tree can have
+        hl -- a list of values
+        """
+
+        if n < 1:
+            raise ValueError("Parameter 'n' must be greater than zero.")
+
         self.__n = n
         self._hl = [] if hl == None else hl[:]
 
@@ -12,15 +26,21 @@ class NHeap(object):
             self.__heapify(i)
     
     def get_n(self):
-        """Get maximum number of children each (sub)tree can have. O(1)."""
+        """
+        Get maximum number of children each (sub)tree can have. O(1).
+        """
         return self.__n
 
     def get_max(self):
-        """Get maximum item, do not remove it. O(1)."""
+        """
+        Get maximum item, do not remove it. O(1).
+        """
         return self._hl[0] if self.size() > 0 else None
 
     def extract_max(self):
-        """Get maximum item and remove it. O(???)."""
+        """
+        Get maximum item and remove it. O(log n).
+        """
         max = self.get_max()
 
         if max != None:
@@ -33,7 +53,9 @@ class NHeap(object):
         return max
 
     def insert(self, value):
-        """Insert new item into heap. O(???)."""
+        """
+        Insert new item into heap. O(log n).
+        """
         self._hl.append(value)
         i = self.size() - 1
 
@@ -42,7 +64,9 @@ class NHeap(object):
             i = self.__parent(i)
 
     def size(self):
-        """Get size of heap. O(1)."""
+        """
+        Get size of heap. O(1).
+        """
         return len(self._hl)
 
     def __heapify(self, i):
@@ -59,7 +83,11 @@ class NHeap(object):
             self._hl[i], self._hl[max] = self._hl[max], self._hl[i]
             self.__heapify(max)
 
-    def __children(self, i):
+    def _children(self, i):
+        """
+        Get range of indexes of children of specified index.
+        O(n) where n is maximum number of children each (sub)tree can have.
+        """
         first_child = (self.__n * i) + 1
 
         if first_child >= self.size():
@@ -72,5 +100,8 @@ class NHeap(object):
         
         return range(first_child, last_child + 1)
 
-    def __parent(self, i):
+    def _parent(self, i):
+        """
+        Get index of parent to the specified index. O(1).
+        """
         return (i - 1) // self.__n

7745bf7b3de554c0f09cc3e8bd49bf06f2278361
Marian Dolinský <mariandolinsky@outlook.com>
2019-09-26 22:21:45 +0200

    Added Unity Project01

---
+++ Unity/Project01/Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Project01
+{
+    public sealed class CapsuleController : MonoBehaviour
+    {
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private float speedMultiplier;
+
+        private void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            characterController.Move(new Vector3(x, 0f, z) * speedMultiplier * Time.deltaTime);
+
+            ////Vector3 position = transform.position;
+            ////position.x += x * speedMultiplier * Time.deltaTime;
+            ////position.z += z * speedMultiplier * Time.deltaTime;
+
+            ////transform.position = position;
+        }
+    }
+}

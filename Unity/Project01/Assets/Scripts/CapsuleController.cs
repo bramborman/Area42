550bde36ae8b3890943c1b4ce31ff1b20d83e400
Marian Dolinský <mariandolinsky@outlook.com>
2019-10-03 19:44:03 +0200

    Many changes in Unity Project01

--- Unity/Project01/Assets/Scripts/CapsuleController.cs
+++ Unity/Project01/Assets/Scripts/CapsuleController.cs
@@ -4,21 +4,70 @@ namespace Project01
 {
     public sealed class CapsuleController : MonoBehaviour
     {
-        [SerializeField] private CharacterController characterController;
-        [SerializeField] private float speedMultiplier;
+        private float rotationY = 0f;
+        private Vector3 move = Vector3.zero;
+        private Vector3 velocity = Vector3.zero;
+
+        [SerializeField] private CharacterController characterController = null;
+        [SerializeField] private Transform shootPoint = null;
+        [SerializeField] private Fireball fireballPrefab = null;
+        [SerializeField] private ParticleSystem impactPrefab = null;
+        [SerializeField] private float speedMultiplier = 5f;
+        [SerializeField] private float rotationSpeedMultiplier = 100f;
 
         private void Update()
         {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
 
-            characterController.Move(new Vector3(x, 0f, z) * speedMultiplier * Time.deltaTime);
+            rotationY += horizontal * Time.deltaTime * rotationSpeedMultiplier;
+            transform.rotation = Quaternion.Euler(0f, rotationY, 0f);
+
+            move.Set(0f, 0f, vertical);
+
+            if (!characterController.isGrounded)
+            {
+                move.y -= 9.81f * Time.deltaTime;
+            }
+
+            move = transform.TransformDirection(move);
+            velocity += move;
+
+            if (velocity.magnitude > 5f)
+            {
+                velocity = Vector3.ClampMagnitude(velocity, 5f);
+            }
+
+            characterController.Move(velocity * speedMultiplier * Time.deltaTime);
+
+            velocity = Vector3.Lerp(velocity, Vector3.zero, 0.05f);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ThrowFireball();
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (Physics.Raycast(shootPoint.position, transform.forward, out RaycastHit hit, 100f))
+                {
+                    ParticleSystem impact = Instantiate(impactPrefab, hit.point, Quaternion.identity);
+                    impact.transform.forward = hit.normal;
+                    impact.Play();
+                }
+            }
 
             ////Vector3 position = transform.position;
             ////position.x += x * speedMultiplier * Time.deltaTime;
             ////position.z += z * speedMultiplier * Time.deltaTime;
 
             ////transform.position = position;
+        }
+
+        private void ThrowFireball()
+        {
+            Fireball fireball = Instantiate(fireballPrefab, shootPoint.position, Quaternion.identity);
+            fireball.Shoot(transform.forward);
         }
     }
 }
---
+++ Unity/Project01/Assets/Scripts/Fireball.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private new Rigidbody rigidbody = null;
+    [SerializeField] private float forceMultiplier = 50f;
+
+    public void Shoot(Vector3 direction)
+    {
+        rigidbody.AddForce(direction * forceMultiplier, ForceMode.Impulse);
+    }
+}
